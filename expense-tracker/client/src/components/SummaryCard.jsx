1b4c09bc23f1379d6d5f29367863848da21d5623
sahajsrivastava044-spec <sahajsrivastava044@gmail.com>
2026-06-15 01:14:58 +0530

    Added some laayout changes and made the app responsive.

--- expense-tracker/client/src/components/SummaryCard.jsx
+++ expense-tracker/client/src/components/SummaryCard.jsx
@@ -1,9 +1,14 @@
 
-function SummaryCard({ title, amount }) {
+function SummaryCard({ title, amount, variant }) {
+  const variants={
+    income: "bg-green-600 text-white",
+    expense: "bg-red-600 text-white",
+    balance: "bg-blue-600 text-white" 
+   }
   return (
-    <div className="border rounded-xl p-6 shadow-md flex-1">
-        <h3 className="text-gray-500 text-sm">{title}</h3>
-        <p className="text-2xl font-bold mt-2">${amount}</p>
+    <div className={`border rounded-xl p-6 shadow-md flex-1 ${variants[variant] || ''}`}>
+        <h3 className="text-gray-500 text-sm text-white">{title}</h3>
+        <p className="text-2xl font-bold mt-2 text-white">₹{amount}</p>
     </div>
   )
 }
